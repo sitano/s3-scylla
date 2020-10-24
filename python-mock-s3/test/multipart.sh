fb60d8673ae72ae52369e54f9c8a3284b725c5c7
Marcin Maliszkiewicz <marcinmal@gmail.com>
2020-10-24 19:34:09 +0200

    improve tests

--- python-mock-s3/test/multipart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# create
-aws --endpoint-url http://localhost:8000 --no-cli-pager s3api create-multipart-upload --bucket marcin --key k000
-
-# ... the rest
