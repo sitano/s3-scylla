f0502f881241731ca7ffe9235d2eec6d08a30dad
Raphael S. Carvalho <raphaelsc@scylladb.com>
2020-10-26 09:53:33 -0300

    Implement Garbage Collection for OACS
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- sstables/object_aware_compaction_strategy.cc
+++ sstables/object_aware_compaction_strategy.cc
@@ -1,6 +1,8 @@
 #include "object_aware_compaction_strategy.hh"
 #include "database.hh"
 #include "sstables.hh"
+#include "service/priority_manager.hh"
+#include <algorithm>
 
 namespace sstables {
 
@@ -48,12 +50,24 @@ object_aware_compaction_strategy::get_sstables_for_compaction(column_family& cf,
 
     objects_map map = group_sstables_by_pk_component(candidates);
 
-    // Compact a set of sstables for a given object if there's a sstable in that set with a tombstone inside
+    // Compact a set of sstables for a given object if there's a sstable in that set with a tombstone inside,
+    // to garbage collect the deleted data from a specific object.
     for (auto& entry : map) {
-        // TODO: return set for object if it has tombstone inside
-        if (true) {
-            return compaction_descriptor();
+        bool has_old_enough_tombstone = std::any_of(entry.second.begin(), entry.second.end(), [this] (const shared_sstable& sst) {
+            bool has_tombstone = sst->get_stats_metadata().estimated_tombstone_drop_time.bin.size() > 0;
+            // age of tombstone is taken into account, to prevent tombstone compaction from being
+            // triggered infinitely
+            if (has_tombstone) {
+                return (db_clock::now()-_tombstone_compaction_interval) < sst->data_file_write_time();
+            }
+            return false;
+        });
+        if (has_old_enough_tombstone) {
+            return compaction_descriptor(std::move(entry.second),
+                                         cf.get_sstable_set(),
+                                         service::get_local_compaction_priority());
         }
+        // TODO: perform STCS if the set of sstables, for a given object, is growing too large???
     }
 
     return compaction_descriptor();
